993ac5ae02efbdd25f1c7cf7ceb057448d2fe6d2
David Kuruvilla <dapawn@gmail.com>
2017-10-20 16:52:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
 			 :known_for => "Ruby",
 		   :languages => ["LISP", "C"]
 		 }
-
+     programmer_hash
 end
 
 def changing_alan
@@ -126,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
